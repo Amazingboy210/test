aa622171ffa4bb9ba0e9c58e28a0c945c2f6be96
Amazingboy210 <100344434+Amazingboy210@users.noreply.github.com>
2022-04-12 22:40:48 +0800

    sql

---
+++ connect_db.php
@@ -0,0 +1,19 @@
+<?php
+
+$debug_log = "";
+$server_name = "LAPTOP-2R02G9RF";
+$connection = array("Database"=>"Lexile", "UID"=>"sa", "PWD"=>"08901160", "CharacterSet"=>"UTF-8");
+$conn = sqlsrv_connect($server_name, $connection);
+if($conn){
+    $debug_log = "connection established!";
+}else{
+    $debug_log = "Failed to connect MS DB";
+    die(print_r(sqlsrv_errors(), true));
+}
+?>
+
+<script>
+    var log_message = "<?php echo $debug_log; ?>";
+    console.log(log_message);
+</script>
+
